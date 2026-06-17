ebad41f68b91e49f15ae6cd2ecdb7cf675d7bde3
Vladislav K <110909601+ch1kim0n1@users.noreply.github.com>
2026-06-17 03:06:50 -0500

    Create final.py

---
+++ final.py
@@ -0,0 +1,20 @@
+from heapq import heappush, heappop
+
+class MedianFinder:
+
+    def __init__(self):
+        self.small = []
+        self.large = []
+
+    def addNum(self, num: int) -> None:
+        heappush(self.small, -num)
+
+        heappush(self.large, -heappop(self.small))
+
+        if len(self.large) > len(self.small):
+            heappush(self.small, -heappop(self.large))
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -self.small[0]
+        return (-self.small[0] + self.large[0]) / 2.0
